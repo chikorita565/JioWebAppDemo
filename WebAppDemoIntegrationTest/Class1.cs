316ca7d67201f080e455602fc5bc558e4a9124ec
Lazaro, Renz Jio N <renzjiolazaro31@gmail.com>
2021-11-10 22:34:25 +0800

    Unit Test.

---
+++ WebAppDemoIntegrationTest/Class1.cs
@@ -0,0 +1,37 @@
+﻿using JioWebAppDemo;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebAppDemoIntegrationTest
+{
+    public class BasicTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        public BasicTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+
+        public async Task GetHttpRequest(string url)
+        {
+            //Arrange
+            var client = _factory.CreateClient();
+
+            //Act
+            var response = await client.GetAsync(url);
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
