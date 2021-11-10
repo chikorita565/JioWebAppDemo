00b3afa29bdd139c3daec36b0ab84102663bcc63
Lazaro, Renz Jio N <renzjiolazaro31@gmail.com>
2021-11-10 11:18:14 +0800

    Maraming VChanges

---
+++ JioWebAppDemo/Data/Migrations/20211108052300_InitialCreated.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JioWebAppDemo.Data.Migrations
+{
+    public partial class InitialCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false),
+                    ScheduledDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    From = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Time = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Airline = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Flight = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Terminal = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
--- JioWebAppDemo/Models/Person.cs
+++ JioWebAppDemo/Models/Person.cs
@@ -10,8 +10,12 @@ namespace JioWebAppDemo.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
+        public DateTime ScheduledDate { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Airline { get; set; }
+        public string Flight { get; set; }
+        public string Terminal { get; set; }
+        public string Status { get; set; }
     }
 }
